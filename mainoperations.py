73a8e5c75f8e4a3d59b2d142dd3dfe11d03674ef
elementalneil <nayek.sankhanil@gmail.com>
2020-10-29 14:04:38 +0530

    Modified mainoperations.py to add a log for every transaction.

--- mainoperations.py
+++ mainoperations.py
@@ -11,7 +11,16 @@ class financeoperations:
                 username VARCHAR2(50) NOT NULL UNIQUE PRIMARY KEY,
                 savings NUMBER(50) NOT NULL,
                 wallet NUMBER(50) NOT NULL
-            )
+            );
+            CREATE TABLE IF NOT EXISTS Logs(
+                username VARCHAR2(50) NOT NULL,
+                isavings NUMBER(50) NOT NULL,
+                iwallet NUMBER(50) NOT NULL,
+                details VARCHAR2 NOT NULL,
+                fsavings NUMBER(50) NOT NULL,
+                fwallet NUMBER(50) NOT NULL,
+                timenow TIME NOT NULL
+            );
         '''
         self.sql.executescript(initscript)
 
@@ -40,29 +49,64 @@ class financeoperations:
         self.sql.execute('COMMIT')
 
     def addMoney(self, account, amount):    #Adds money to specified account 's' or 'w'
+        isavings=self.savingsBalance
+        iwallet=self.walletBalance
+
         if account=='s':
             self.savingsBalance+=amount
+            details='Added '+str(amount)+' to savings.'
         else:
             self.walletBalance+=amount
+            details='Added '+str(amount)+' to wallet.'
+
+        fsavings=self.savingsBalance
+        fwallet=self.walletBalance
+
+        self.updatelog(isavings, iwallet, details, fsavings, fwallet)
         self.updateDB()
 
     def spendMoney(self, account, amount):  #Spends money from specified account 's' or 'w'
+        isavings=self.savingsBalance
+        iwallet=self.walletBalance
+
         if account=='s':
             self.savingsBalance-=amount
+            details='Spent '+str(amount)+' from savings.'
         else:
             self.walletBalance-=amount
+            details='Spent '+str(amount)+' from wallet.'
+
+        fsavings=self.savingsBalance
+        fwallet=self.walletBalance
+
+        self.updatelog(isavings, iwallet, details, fsavings, fwallet)
         self.updateDB()
 
-
     def moveMoney(self, account, amount):   #Moves money from specified account to the other
+        isavings=self.savingsBalance
+        iwallet=self.walletBalance
+
         if account=='s':
             self.savingsBalance-=amount
             self.walletBalance+=amount
+            details='Moved '+str(amount)+' from savings to wallet.'
         else:
             self.walletBalance-=amount
             self.savingsBalance+=amount
+            details='Moved '+str(amount)+' from wallet to savings.'
+
+        fsavings=self.savingsBalance
+        fwallet=self.walletBalance
+
+        self.updatelog(isavings, iwallet, details, fsavings, fwallet)
         self.updateDB()
 
+    def updatelog(self, isavings, iwallet, details, fsavings, fwallet):
+        self.sql.execute('''
+                INSERT INTO Logs
+                VALUES (?, ?, ?, ?, ?, ?, (SELECT date('now')))
+            ''',(self.username, isavings, iwallet, details, fsavings, fwallet))
+        self.sql.execute('COMMIT')
 
 #Testing Block for This Class
 if __name__=='__main__':
